66519119702769462fc482c6ad0fc1c7f033e301
Hinna <hinna@HinnadeMacBook-Air.local>
2025-08-02 00:08:07 +0800

    update

--- R/RA_demo.r
+++ R/RA_demo.r
@@ -1,17 +1,19 @@
+## load target ata
 setwd("./github/CALACD/R")
 acc <- readRDS("../data/acc_data.rds")
 colnames(acc)[1] <- "participant_id"
 
+## load qc data
 source("data_qc.r")
 cali_qc <- readRDS("../data/calibration_qc.rds")
 wear_qc <- readRDS("../data/wear_qc.rds")
 
+## runqc
 res <- qc_subset(wear_qc, cali_qc)
 id <- res$sub_id
 
+## run RA calculation
 source("RA_calculation.r")
 ddd <- subset(acc, acc$participant_id %in% id)
-
-source("RA_calculation.r")
 res <- calculate_RA(ddd, id_column = "participant_id", hour_prefix = "Average acceleration")
 res |> head()
